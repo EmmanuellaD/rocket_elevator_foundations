cc168a27f466beef16f90d22be90b9f2bb431c32
Emmanuellad <emmanuella0524@yahoo.com>
2020-11-19 21:33:59 -0500

    deploy

--- config/initializers/db_second.rb
+++ config/initializers/db_second.rb
@@ -6,4 +6,5 @@
 # config =YAML::load(ERB.new(File.read(Rails.root.join("config","database_second.yml"))).result)
 # DB_SECOND = config[Rails.env].
 
-DB2 = YAML.load_file(File.join(Rails.root, "config", "database_second.yml"))[Rails.env.to_s]
+#  DB2 = YAML.load_file(File.join(Rails.root, "config", "database_second.yml"))[Rails.env.to_s]
+ DB2= YAML::load(ERB.new(File.read(Rails.root.join("config","database_second.yml"))).result)[Rails.env]
