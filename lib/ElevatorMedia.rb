7c034fefdfd88ca08cae927d4005ead573c51450
Emmanuella <emmanuella0524@yahoo.com>
2021-01-14 16:20:54 -0500

    joke remove

--- lib/ElevatorMedia.rb
+++ lib/ElevatorMedia.rb
@@ -1,5 +1,15 @@
 module ElevatorMedia
     class Streamer
+
+#         http.use_ssl = true
+# http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+# request = Net::HTTP::Get.new(url)
+# request["x-rapidapi-key"] = 'f96f2510acmsh4237e8c7e65d1f3p1a6b35jsn82bb21db9482'
+# request["x-rapidapi-host"] = 'covid-19-coronavirus-statistics.p.rapidapi.com'
+
+# response = http.request(request)
+# puts response.read_body
         def getContent()
             url = URI("https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json&blacklistFlags=nsfw%2Cracist&idRange=0-150&type=single")
 
@@ -13,7 +23,7 @@ module ElevatorMedia
 
             response = http.request(request)
             result=JSON.parse(response.read_body)['joke']
-            return "<div><h3>Joke: #{result}</h3></div"
+            return "<div><h3> #{result}</h3></div"
          end
         end
     end
